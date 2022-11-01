5f5f125e347224ff21c833d2b4ae5fdd4cae4863
SunneyG <avocado34.131@gmail.com>
2022-11-01 15:08:43 +0900

    Rename: Restructure test files

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -24,7 +24,7 @@ let package = Package(
             dependencies: [], path: "Sources/"),
         .testTarget(
             name: "BISegmentedControlTests",
-            dependencies: [], path: "Example/Tests/"),
+            dependencies: [], path: "Tests/"),
     ],
     swiftLanguageVersions: [.v5]
 )
